d27778312f90a0efa09dee92457a9fad23cf5368
KorayKaya <korre.remix@gmail.com>
2021-08-27 21:18:58 +0200

    big step forward, feed ok

---
+++ src/custom/ProfileContent.js
@@ -0,0 +1,15 @@
+import "./ProfileContent.css";
+
+export default function ProfileContent(props) {
+    function follow(){
+        
+    }
+    return (
+        <div class="profilecontent">
+            <h2 className="profilename">
+                Koray
+            </h2>
+            <button>Follow</button>
+        </div>
+    )
+}
